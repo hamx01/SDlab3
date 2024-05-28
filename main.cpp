ae3b8e89af18ccadca67dbf902aa07400b3c15a2
Andrii Solianyk <asolianik2015@gmail.com>
2024-05-28 15:55:09 +0200

    Usunięto niepotrzebne rzeczy

--- main.cpp
+++ main.cpp
@@ -19,19 +19,10 @@ std::pair<std::vector<int>, std::vector<int>> bellman_ford(unsigned int start, c
             }
         }
     }
-
-    for (int u = 0; u < n; ++u) {
-        for (int v = 0; v < n; ++v) {
-            if (adjacency_matrix[u][v] != 0 && dist[u] != std::numeric_limits<int>::max() && dist[u] + adjacency_matrix[u][v] < dist[v]) {
-                throw std::runtime_error("Graf zawiera cykl o ujemnej wadze");
-            }
-        }
-    }
-
     return {dist, pred};
 }
 
-std::vector<int> reconstruct_path(int start, int end, const std::vector<int>& pred) {
+std::vector<int> reconstruct_path(int end, const std::vector<int>& pred) {
     std::vector<int> path;
     for(int v = end; v != -1; v = pred[v]) {
         path.push_back(v);
@@ -41,15 +32,6 @@ std::vector<int> reconstruct_path(int start, int end, const std::vector<int>& pr
 }
 
 int main() {
-//    std::vector<std::vector<int>> adjacency_matrix = {
-//            {0, 2, 0, 0, 1, 0},
-//            {2, 0, 3, 0, 4, 0},
-//            {0, 3, 0, 1, 0, 0},
-//            {0, 0, 1, 0, 2, 2},
-//            {1, 4, 0, 2, 0, 0},
-//            {0, 0, 0, 2, 0, 0}
-//    };
-
     std::vector<std::vector<int>> adjacency_matrix = {
           // 1  2  3  4  5  6  7  8  9 10 11 12 13
             {0, 2, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // 1
@@ -82,10 +64,9 @@ int main() {
     auto [dist, pred] = bellman_ford(start, adjacency_matrix);
 
     std::cout << "-----------------------------------------------------------" << std::endl;
-//    for(int i = 0; i < dist.size(); i++) {
         if(end != start) {
             std::cout << "Optymalna traektoria sterowan od " << start+1 << " do " << end+1 << " wynosi: ";
-            auto path = reconstruct_path(start, end, pred);
+            std::vector<int> path = reconstruct_path(int(end), pred);
             for(int v : path) {
                 std::cout << v+1 << " ";
             }
@@ -93,12 +74,10 @@ int main() {
             std::cout << "Wskaznik jakosci J: " << dist[end] << std::endl;
             std::cout << "-----------------------------------------------------------" << std::endl;
         } else {
-//            std::cout << "-----------------------------------------------------------" << std::endl;
             std::cout << "Optymalna traektoria sterowan od " << start+1 << " do " << end+1 << " wynosi: " << start+1 << std::endl;
             std::cout << "Wskaznik jakosci J: " << start+1 << std::endl;
             std::cout << "-----------------------------------------------------------" << std::endl;
         }
-//    }
 
     return 0;
 }
